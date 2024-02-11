63e0670c3c180648e1f3b3d368b634e8cf32e133
dev-dori <trouna43@gmail.com>
2024-02-11 16:24:02 +0000

    Improved application size control function

--- src/components/Window/index.js
+++ src/components/Window/index.js
@@ -10,30 +10,33 @@ function Window({Name, Contents, Update, app,WindowSize}){
     const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
     const {key, focused, zIndex} = app;
     const {WindowHeight,WindowWidth} = WindowSize;
-    const [[maximized,fix_maximized], setMaximized] = useState([false,false]);
+    let [[maximized,fix_maximized], setMaximized] = useState([false,false]);
     const [minimized, setMinimized] = useState(false);
     const [resizing, setResizing] = useState(false);
+    const [WindowResizeing, setWindowResizeing] = useState(false);
     const [moving, setMoving] = useState(false);
     const navigate = useNavigate();
-    useEffect(() => {
-        window.addEventListener("resize", setResizing);
-        if (focused && minimized) {
-            setMinimized(false);
-        }
-    }, [focused]);
-    const [[left, top, width, height], setCoords] = useState([getRandom((window.innerWidth-WindowWidth)/6, (window.innerWidth-WindowWidth)/1.5), 
+
+    let [[left, top, width, height], setCoords] = useState([getRandom((window.innerWidth-WindowWidth)/6, (window.innerWidth-WindowWidth)/1.5), 
                                                               getRandom(30,(window.innerHeight-WindowHeight)-70), 
                                                               WindowWidth, 
                                                               WindowHeight]);
+    useEffect(() => {
+        window.addEventListener("resize", setWindowResizeing);
+        if (focused && minimized) setMinimized(false);
+    }, [focused]);
 
-    if (!maximized && ((window.innerHeight<=500 || window.innerWidth<=500)||width+left>window.innerWidth||height+top>window.WindowHeight)) setMaximized([true,true])
-    else if (fix_maximized && !((window.innerHeight<=500 || window.innerWidth<=500)||width+left>window.innerWidth||height+top>window.WindowHeight)) setMaximized([false,false])
-   
+    if(!maximized && left+width>window.innerWidth && window.innerWidth-width>=0) left=window.innerWidth-width
+    if(!maximized && top+height>window.innerHeight && window.innerHeight-height>=0) top=window.innerHeight-height
+    if (!maximized && ((window.innerHeight<=height || window.innerWidth<=width ))) setMaximized([true,true])
+    else if (fix_maximized && !((window.innerHeight<=height || window.innerWidth<=width ))) setMaximized([false,false])
+
     return (
         <div className={classes('Window',key,resizing && 'resizing',focused && 'focused', moving && 'moving',  minimized && 'minimized', maximized && 'maximized')}
             style={{ left, top, width, height, zIndex}}
             onMouseDown={e=>{if(!focused) navigate(key);}}
             onWheel={e=>{if(!focused) navigate(key);}}
+            onDoubleClick={e=>{setMaximized([!maximized, false])}}
         >
             {/* ############    상단바     ############ */}
             <div className='toolbar' 
@@ -42,16 +45,25 @@ function Window({Name, Contents, Update, app,WindowSize}){
                     const offsetY = e.clientY;
                     setMoving(true);
                     const onMouseMove = e => {
+                        if(fix_maximized) return ;
                         const dx = e.clientX - offsetX;
                         const dy = e.clientY - offsetY;
-                        console.log(top + dy)
 
-                        if(top+dy<0) setMaximized([true, false]);
-                        else setMaximized([false, false]);
+                        if(top+dy<0) setMaximized([true, false]); // 어플리케이션을 최상단으로 드래그시 전체화면으로 변경 
+                        else if (maximized){  //전체 화면에서 상단바를 드래그 할때
+                            // 커서의 x좌표가 어플리케이션 너비의 절반값을 화면너비의 시작과 끝에서 제외한 범위 안에 들어왔을때 바가 커서 중앙으로 고정
+                            if(width/2<e.clientX&&e.clientX<window.innerWidth-width/2) left = e.clientX-width/2 
+                            // 커서의 x좌표가 좌측을 기준으로 어플리케이션 너비안에 들어왔을 경우 바 좌표를 왼쪽으로 고정
+                            else if (e.clientX<width/2) left = 0
+                            // 커서의 x좌표가 우측을 기준으로 어플리케이션 너비안에 들어왔을 경우 바 좌표를 오른쪽으로 고정
+                            else left = window.innerWidth-width
+                            top=0; maximized=false
+                            setMaximized([false, false]);
+                        }else setMaximized([false, false]); // 커서를 놓지 않은 상태에서 어플리케이션바의 좌표가 최상단을 벗어났을때 전체화면 해제
                         
                         setCoords([
                           (left+dx >= 0 && left + dx + width <= window.innerWidth ? left + dx:(left+dx<=0?0:window.innerWidth-width)),
-                          (top +dy >= 0 && top  + dy + height <= window.innerHeight ? top + dy:(top+dy<=0?0:window.innerHeight-height)),
+                          (top+dy >= 0 && top  + dy + height <= window.innerHeight && top+dy>=0 ? top + dy:(top+dy<=0?0:window.innerHeight-height)),
                           (width),
                           (height),
                         ]);
@@ -105,12 +117,12 @@ function Window({Name, Contents, Update, app,WindowSize}){
                                     let newTop = top;
                                     let newWidth = width;
                                     let newHeight = height;
-                                    
-                                    console.log("offsetY : ",offsetY,"dy : ",dy,"newTop : ",newTop,"e.clientY : ",e.clientY)
 
                                     sides.forEach(side => {
                                         switch (side) {
                                             case 'top':
+                                                if(dy+top<0) setMaximized([true, false]);
+                                                else setMaximized([false, false]);
                                                 newTop = (dy + top >= 0) ? top+dy : 0;
                                                 newHeight = (dy + top >= 0) ? newHeight-dy:newHeight+offsetY;
                                                 newTop = newHeight>300?newTop:offsetY+height-300
