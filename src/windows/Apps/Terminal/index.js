eb6f9b040d87e4db3b91295406ecb5fc0a177570
dev-dori <trouna43@gmail.com>
2024-05-06 10:29:25 +0000

    Add new command prompt function

--- src/windows/Apps/Terminal/index.js
+++ src/windows/Apps/Terminal/index.js
@@ -1,18 +1,78 @@
 import { Window } from 'components';
 import { ComingSoon } from 'windows';
+import { useState, useEffect, useRef} from 'react';
+import './style.scss';
 
 function Terminal(props){
     const { Update, app } = props;
     let WindowSize = {WindowHeight:450, WindowWidth:700};
-    return(<ComingSoon Update={Update} app={app}/>)
+    const scrollRef = useRef();
+    const [command, setCommand] = useState("");
+    const [terminalCommandData, setTerminalCommandData] = useState([
+        { type: "output", value: "Welcome to the DevDori World !👋" },
+        { type: "output", value: "Type \"help\" for all available commands."}
+      ]);
+    
+    const CommandList = {
+        "help":"help  \t\tshow all the possible commands\
+              \nwhoami\t\tdisplay information about DevDori\
+              \nclear \t\tclear the terminal screen\
+              ",
+        "whoami":"devdori"
+    }
 
+    
+    useEffect(()=>{
+        scrollToBottom();
+        window.addEventListener("keydown",handleKeyDownEvent)
+        return () => window.removeEventListener("keydown",handleKeyDownEvent);
+    },[command,terminalCommandData])
+
+
+    const scrollToBottom = () => {
+        console.log(scrollRef.current.scrollTop )
+        if (scrollRef.current) {
+            scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+        }
+    };
+
+    const handleKeyDownEvent = (event) => {
+        if(!app.focused) return;
+        if(event.key==="Enter") {
+            execCommand(command)
+            setCommand("")
+        }else if(event.key&&event.key.length===1) setCommand(command+event.key)
+        else if(event.key==="Backspace") setCommand(command.slice(0, -1))
+    }
+    
+    const execCommand = (cmd) => {
+        let cmdData = [
+            ...terminalCommandData,
+            {type:"input", value:cmd},
+            {type:"output",value:CommandList[cmd]?CommandList[cmd]:cmd&&"-bash: "+cmd+": command not found"}
+        ]
+        if(cmd==="clear") cmdData=[]
+        setTerminalCommandData(cmdData)
+    }
+
+    // return(<ComingSoon Update={Update} app={app}/>)
     return(
         <Window 
         Update = {Update}
         app = {app}
         WindowSize = {WindowSize}
         ClassName='Window Terminal'
-        Contents={(<div>Terminal</div>)}
+        Contents={(
+            <div className='Terminal-Body' ref={scrollRef}>
+                {
+                    // console.log(typeof(terminalCommandData))
+                    Object.entries(terminalCommandData).map(([key]) => 
+                        <div key={key}>{(terminalCommandData[key].type=="input"?"$ ":"")+terminalCommandData[key].value}</div>
+                    )
+                }
+                <div className='terminal-input'><span>$ {command}</span>{app.focused&&<span className='cursor'/>}</div>
+            </div>
+        )}
     />);
 
 }
