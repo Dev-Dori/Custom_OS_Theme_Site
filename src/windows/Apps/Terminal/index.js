4b166e1e2885c5a63847f2a7c5c5ed345cd1688b
dev-dori <trouna43@gmail.com>
2024-05-09 11:49:19 +0000

    Add clear and history commands to the terminal

--- src/windows/Apps/Terminal/index.js
+++ src/windows/Apps/Terminal/index.js
@@ -8,7 +8,10 @@ function Terminal(props){
     let WindowSize = {WindowHeight:450, WindowWidth:700};
     const scrollRef = useRef();
     const [command, setCommand] = useState("");
+    const [user,setUser] = useState("DevDori")
+    const [workDir, setWorkDir] = useState("~")
     const [terminalCommandData, setTerminalCommandData] = useState([
+        { type: "clear", value:0},
         { type: "output", value: "Welcome to the DevDori World !👋" },
         { type: "output", value: "Type \"help\" for all available commands."}
       ]);
@@ -18,7 +21,7 @@ function Terminal(props){
               \nwhoami\t\tdisplay information about DevDori\
               \nclear \t\tclear the terminal screen\
               ",
-        "whoami":"devdori"
+        "whoami":user
     }
 
     
@@ -30,7 +33,6 @@ function Terminal(props){
 
 
     const scrollToBottom = () => {
-        console.log(scrollRef.current.scrollTop )
         if (scrollRef.current) {
             scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
         }
@@ -45,13 +47,21 @@ function Terminal(props){
         else if(event.key==="Backspace") setCommand(command.slice(0, -1))
     }
     
+
     const execCommand = (cmd) => {
-        let cmdData = [
-            ...terminalCommandData,
-            {type:"input", value:cmd},
-            {type:"output",value:CommandList[cmd]?CommandList[cmd]:cmd&&"-bash: "+cmd+": command not found"}
-        ]
-        if(cmd==="clear") cmdData=[]
+        let cmdData= []
+        if(CommandList[cmd]) cmdData = [...terminalCommandData,{type:"input", value:cmd},{type:"output",value:CommandList[cmd]}]
+        else if(cmd==="history"){
+            cmdData = [...terminalCommandData,
+                        {type:"input", value:cmd},
+                        {type:"output",value:terminalCommandData.filter((tmpCmdData)=>{return tmpCmdData.type==="input"&&tmpCmdData.value}).map(tmpCmdData=>tmpCmdData.value).concat("history").join("\n")}
+                      ]
+        }else if(cmd==="clear"){
+            cmdData = [...terminalCommandData,
+                {type:"input", value:cmd}
+            ]
+            cmdData[0].value = terminalCommandData.length
+        }else cmdData = [...terminalCommandData,{type:"input", value:cmd},{type:"output",value:cmd&&"-bash: "+cmd+": command not found"}]
         setTerminalCommandData(cmdData)
     }
 
@@ -66,11 +76,14 @@ function Terminal(props){
             <div className='Terminal-Body' ref={scrollRef}>
                 {
                     // console.log(typeof(terminalCommandData))
-                    Object.entries(terminalCommandData).map(([key]) => 
-                        <div key={key}>{(terminalCommandData[key].type=="input"?"$ ":"")+terminalCommandData[key].value}</div>
-                    )
+                    Object.entries(terminalCommandData).map(([key]) => { 
+                        if(terminalCommandData[key].type==="input" && key>terminalCommandData[0].value)
+                            return (<div key={key}>{user}@world:{workDir}$ {terminalCommandData[key].value}</div>)
+                        else if(terminalCommandData[key].type==="output" && key>terminalCommandData[0].value)
+                            return (<div key={key}>{terminalCommandData[key].value}</div>)
+                    })
                 }
-                <div className='terminal-input'><span>$ {command}</span>{app.focused&&<span className='cursor'/>}</div>
+                <div className='terminal-input'><span>{user}@world:{workDir}$ {command}</span>{app.focused&&<span className='cursor'/>}</div>
             </div>
         )}
     />);
