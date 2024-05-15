d79010079073692c9e2ffb563c7888c1f0d8bc58
dev-dori <trouna43@gmail.com>
2024-05-15 08:28:52 +0000

    Unnecessary component repeat phenomenon patch

--- src/components/Desktop/index.js
+++ src/components/Desktop/index.js
@@ -12,45 +12,7 @@ function Desktop(){
     const [FileSystem]  = useContext(FileSystemContext);
     const GetDesktop = FileSystem.GetDesktopDir();
     const [apps,GetApp] = useState(FileSystem.GetApps());
-    console.log(GetDesktop)
-
-    const navigate = useNavigate();
-    const location = useLocation();
-    let currentUrl = location.pathname;
-
-    const refresh=()=>{
-        let change = false;
-        for(let key in apps){
-            // URL에서 호출된 정보를 기준으로 포커스 여부 판단 
-            const focused = (currentUrl.split("/")[1] === key);
-            if (currentUrl.split("/")[1]===""&&apps[key].focused&&!apps[key].closing){
-                apps[key].minimized = true;
-                apps[key].focused = false;
-            } else apps[key].minimized = false;
-            // 창이 닫힌 상태 -> 새로 열기
-            if (apps[key].closing) {
-                apps[key].closing = false;
-                apps[key].opened = false;
-                apps[key].focused = false;
-            }
-            // 창이 열려있는 상태 -> 다시 창을 클릭 한 경우
-            if (focused && (!apps[key].opened||!apps[key].focused)){
-                apps[key].opened = true;
-                apps[key].zIndex = Math.max(...Object.keys(apps).map(app => apps[app].zIndex)) + 1;
-                change = true;
-            }
-            apps[key].focused = focused; // 무한반복을 방지하기 위함
-        }
-    }
-    
-    useEffect(() => {refresh()});
-    useEffect(() => {
-        window.addEventListener("keydown", (event)=>{if(event.key=="Escape"&&window.location.pathname!="/"){navigate("/")}})
-        // return () => {arguments.callee}
-    },[])
-
-
-    
+   
     return (
         <div className='Desktop'>
             {/* <div className="Wallpaper" style={{backgroundImage: `linear-gradient(to top, rgba(19, 21, 25, 0.5), rgba(19, 21, 25, 0.5)),url(${background})`}}></div> */}
@@ -77,7 +39,6 @@ function Desktop(){
                         let application = Object.assign(apps[key],{"key":key});
                         return(<application.WindowComponent key={application.name} app={application}
                             Update={patch =>{
-                                console.log(patch)
                                 Object.assign(application, patch);
                                 GetApp(apps)
                             ;}}
--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -4,19 +4,52 @@ import { Desktop,Taskbar } from 'components';
 import { FileSystemContext } from 'contexts'
 import { RootDir } from 'beans';
 import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'
+import { useNavigate,useLocation } from 'react-router-dom'
 import './style.scss'
 
 function Screen(){
-    const [reload, SetReload] = useState(0);
     const [FileSystem, SetFileSystem] = useState(RootDir.instance)
-    const location = useLocation()
+    const navigate = useNavigate();
 
-    useEffect(() => {SetReload(reload+1)}, [location])
-    console.log("=========Reload=========")
+
+    const [apps,GetApp] = useState(FileSystem.GetApps());
+
+    const location = useLocation();
+    let currentUrl = location.pathname;
+
+    console.log("=== Screen 렌더링 ===",location.pathname)
+
+    useEffect(()=>{
+        const Escape=(event)=>{ if(event.key==="Escape"&&location.pathname!=="/") navigate("/")}
+        window.addEventListener("keydown", Escape)
+        return () => window.removeEventListener("keydown", Escape)
+    },[location])
+
+    for(let key in apps){
+        // URL에서 호출된 정보를 기준으로 포커스 여부 판단 
+        const focused = (currentUrl.split("/")[1] === key);
+        if (currentUrl.split("/")[1]===""&&apps[key].focused&&!apps[key].closing){
+            apps[key].minimized = true;
+            apps[key].focused = false;
+        } else apps[key].minimized = false;
+        // 창이 닫힌 상태 -> 새로 열기
+        if (apps[key].closing) {
+            apps[key].closing = false;
+            apps[key].opened = false;
+            apps[key].focused = false;
+        }
+        // 창이 열려있는 상태 -> 다시 창을 클릭 한 경우
+        if (focused && (!apps[key].opened||!apps[key].focused)){
+            apps[key].opened = true;
+            apps[key].zIndex = Math.max(...Object.keys(apps).map(app => apps[app].zIndex)) + 1;
+        }
+        apps[key].focused = focused; // 무한반복을 방지하기 위함
+    }
+
+
+
     return (
         <div className='Screen'>
-            {/* <FileSystemContext.Provider value={[RootDir.instance, ReloadDir]}> */}
             <FileSystemContext.Provider value={[FileSystem, SetFileSystem]}>
                 <Desktop />
                 <Taskbar />
