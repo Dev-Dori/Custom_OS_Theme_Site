e95499e2116d6061cf95b702f0536b479697bd64
dev-dori <trouna43@gmail.com>
2024-05-16 12:01:26 +0000

    Chage App type

--- src/beans/App/App.js
+++ src/beans/App/App.js
@@ -2,22 +2,24 @@ import { none } from 'images';
 
 let count = 1;
 
-function App(name, WindowComponent,PinTaskbar, icon){ 
-    const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
-    this.type = "App"
-    this.name = name;
-    this.WindowComponent = WindowComponent;
-    this.zIndex = 1;
-    this.opened = false;
-    this.focused = false;
-    this.closing = false;
-    this.minimized = false;
-    this.maximized = false;
-    this.WindowHeight = 750;
-    this.WindowWidth = 450;
-    this.PinTaskbar = PinTaskbar||false;
-    this.icon = icon? icon : none;
-    count++;
+
+class App {
+    constructor(name, WindowComponent,PinTaskbar, icon) {
+        this.type = "App"
+        this.name = name;
+        this.WindowComponent = WindowComponent;
+        this.zIndex = 1;
+        this.opened = false;
+        this.focused = false;
+        this.closing = false;
+        this.minimized = false;
+        this.maximized = false;
+        this.WindowHeight = 750;
+        this.WindowWidth = 450;
+        this.PinTaskbar = PinTaskbar||false;
+        this.icon = icon? icon : none;
+        count++; 
+    }
 }
 
 export default App;
