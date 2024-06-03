a57e988af260087268f5b27673502b814346ab86
dev-dori <trouna43@gmail.com>
2024-06-03 15:23:34 +0000

    TaskBar : Change design-2

--- src/components/Taskbar/index.js
+++ src/components/Taskbar/index.js
@@ -3,29 +3,29 @@ import Timer from './time.js'
 import { useContext,useState,useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Icon } from 'components'; 
+import { CgMenuGridR as Menu } from "react-icons/cg";
 import { classes } from 'common/utils';
 import './style.scss'
 
 function Taskbar(){
-    const [FileSystem,ReloadDir] = useContext(FileSystemContext);
     const location = useLocation();
     const currentUrl = location.pathname;
+    const [FileSystem,ReloadDir] = useContext(FileSystemContext);
     const apps = FileSystem.GetApps();
-
     return (
         <div className="Taskbar">
+            <Menu className='Menu'/>
             <div className='UserName'>DevDori</div>
             <div className='shortcut-container'>
-                {apps&&Object.keys(apps).filter(key=>apps[key].opened ).map(key=>{ // || apps[key].PinTaskbar
+                {apps&&Object.keys(apps).filter(key=>apps[key].opened).map(key=>{ //|| apps[key].PinTaskbar
                     return (
-                        <Link key={'shortcut-'+key} className={classes('shortcut','pinned',apps[key].focused&&'focus')}
+                        <Link key={'shortcut-'+key} className={classes('shortcut','pinned',apps[key].opened&&'active')}
                             to={key===currentUrl.split("/")[1]?"/":key}>
                             <Icon iconKey={key} iconUrl={apps[key].icon}/>
                         </Link>
                         )
                     }
                 )}
-
             </div>
             <Timer />
         </div>
