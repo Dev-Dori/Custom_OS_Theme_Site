aeba6007295ec865f6e16fec776d8b172b788ba5
dev-dori <trouna43@gmail.com>
2024-06-09 01:28:11 +0000

    Taskbar -> Menu : Add Application Icon

--- src/components/Taskbar/Extention/Menu/index.js
+++ src/components/Taskbar/Extention/Menu/index.js
@@ -1,12 +1,18 @@
+import { FileSystemContext, DeviceClassification } from 'contexts'
+import { Icon } from 'components';
 import { FaSearch as SearchIcon } from "react-icons/fa";
 import { CgMenuGridR as MenuIcon } from "react-icons/cg";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useNavigate,useLocation } from 'react-router-dom'
 import './style.scss'
 
 function Menu({menuOpen, setMenuOpen}){
     const [location, setLocation] = useState(useLocation())
+    const [FileSystem, reload] = useContext(FileSystemContext)
     const prelocation = useLocation()
+    const navigate = useNavigate();
+
+    const app = FileSystem.GetApps()
 
     useEffect(()=>{
         if(prelocation !== location){
@@ -17,10 +23,21 @@ function Menu({menuOpen, setMenuOpen}){
 
     return(
         <>
-            {menuOpen && <div className='Menu' onClick={()=>setMenuOpen(!menuOpen)}>
+            {menuOpen && <div className='Menu'>
                 <div className='Search-Box'>
                     <SearchIcon/>
                     <input placeholder='Search' className='Search'/>
+                </div>
+
+                <div className='Application-Box'>
+                    {Object.keys(app).map(key=>{
+                        return (
+                            <div onClick={()=>navigate(key)}>
+                                <Icon iconUrl={app[key].icon}/>
+                                <div className='FileName'>{key}</div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>}
             <MenuIcon className='MenuIcon' onClick={()=>setMenuOpen(!menuOpen)}/>
