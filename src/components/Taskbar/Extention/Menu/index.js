5fbbdbb896efe8706cc4f97ea8b60b95fc9b90d8
dev-dori <trouna43@gmail.com>
2024-06-16 10:55:21 +0000

    TaskBar > Menu : Change to scroll according to the screen size

--- src/components/Taskbar/Extention/Menu/index.js
+++ src/components/Taskbar/Extention/Menu/index.js
@@ -24,23 +24,25 @@ function Menu({menuOpen, setMenuOpen}){
 
     return(
         <>
-            {menuOpen && <div className='Menu'>
+            {menuOpen && (<div className='Menu'>
+                <div className='interceptor' onClick={()=>setMenuOpen(!menuOpen)}></div>
                 <div className='Search-Box'>
                     <SearchIcon/>
-                    <input placeholder='Search' className='Search' onKeyUp={(event)=>setSearch(event.target.value)}/>
+                    <input type="text" placeholder='Search' className='Search' onKeyUp={(event)=>setSearch(event.target.value)}
+                           autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false"/>
                 </div>
 
                 <div className='Application-Box'>
                     {Object.keys(app).map(key=>{
                         return (app[key] instanceof App) && (
-                            <div onClick={()=>navigate(key)}>
+                            <div onClick={()=>navigate(key)} key={`TaskBar-Menu-${key}`}>
                                 <Icon iconUrl={app[key].icon}/>
                                 <div className='FileName'>{key}</div>
                             </div>
                         )
                     }).filter(x=>x)}
                 </div>
-            </div>}
+            </div>)}
             <MenuIcon className='MenuIcon' onClick={()=>setMenuOpen(!menuOpen)}/>
         </>
     )
