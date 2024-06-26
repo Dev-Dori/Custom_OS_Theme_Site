7ff53802167e84d079c22337580687edefb1b104
dev-dori <trouna43@gmail.com>
2024-06-26 06:15:43 +0000

    RootDir : Change System SymbolicLink file on  desktop to Link File

--- src/beans/Directory/RootDir.js
+++ src/beans/Directory/RootDir.js
@@ -11,13 +11,15 @@ class RootDir{
                         system:new App(System,{PinTaskbar:false, icon:IconMap.system, SymbolicLink:{use:true, name: "System"}, WindowSize:{width:700, height:500}}),
                         FileManager:new App(FileManager,{PinTaskbar:true, icon:IconMap.FileManager, SymbolicLink:{use:true, name: "Files"}, WindowSize:{width:650, height:400}}),
                         terminal:new App(Terminal,{PinTaskbar:true, icon:IconMap.termianl, SymbolicLink:{use:true, name: "Terminal"}}),
-                        browser:new App(Browser,{PinTaskbar:true, icon:IconMap.browser, SymbolicLink:{use:true, name: "Blog"}}),
+                        browser:new App(Browser,{PinTaskbar:true, icon:IconMap.browser, SymbolicLink:{use:false, name: "Browser"}}),
                         projects:new App(Project,{PinTaskbar:true, icon:IconMap.project, SymbolicLink:{use:false}}),
                     }
 
         const Desktop = {
                         // APP에 심볼링크 사용으로 생성한 객체는 자동으로 로드됨.
-                        Github: new LinkFile('https://github.com/Dev-Dori',IconMap.github), // Github Icon
+                        Blog: new LinkFile({url:'https://blog.devdori.com/'},IconMap.browser), // Github Icon
+                        Github: new LinkFile({url:'https://github.com/Dev-Dori', newTab:true},IconMap.github), // Github Icon
+                        System: new LinkFile({url:'https://blog.devdori.com/profile'},IconMap.system), // Github Icon
                     }
 
         this.rootDir = new SystemDir({
