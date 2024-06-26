7ce74c9141185ee6e1cb147089150e043088dadf
dev-dori <trouna43@gmail.com>
2024-06-26 06:08:59 +0000

    Add link file type properties

--- src/beans/App/LinkFile.js
+++ src/beans/App/LinkFile.js
@@ -4,7 +4,8 @@ class LinkFile extends ExtensionFunction{
     constructor(href, Icon) {
         super()
         this.type = "LinkFile"
-        this.href = href;
+        this.href = href.url;
+        this.newTab = href.newTab?true:false;
         this.icon = Icon;
     }
 }
