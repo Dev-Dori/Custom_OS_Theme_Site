89c0d1e3aead82ada98461d30b66db91d78f3f24
dev-dori <trouna43@gmail.com>
2024-06-26 06:12:56 +0000

    Browser : Change to open in a new tab when user click a link file

--- src/windows/Apps/Browser/index.js
+++ src/windows/Apps/Browser/index.js
@@ -1,15 +1,32 @@
 import { Window } from 'components';
 import { Arrow, Refresh, Close } from './NavigationIcons'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 import './style.scss'
+import { useEffect, useMemo, useState } from 'react';
 
-function Browser(props, url_path){
+function Browser(props){
     const {Update, app} = props;
+    const [serchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+    const [focusUrl, setFocusUrl] = useState(serchParams.get("url") ? serchParams.get("url") : "https://www.google.com/webhp?igu=1")
+    const [tabList, setTabList] = useState({})
     let WindowSize = {WindowHeight:450, WindowWidth:700};
 
-    // const url=url_path.length?url_path:"https://www.google.com/webhp?igu=1";
-    const url=url_path.length?url_path:"https://blog.devdori.com/";
-    // const url=url_path.length?url_path:"https://github.com/dev-dori";
-    
+    useMemo(()=>{
+        window.history.pushState(null, null, "browser");
+        const tmpurl = serchParams.get("url")
+        if(tmpurl && tmpurl!==focusUrl) setFocusUrl(tmpurl);
+        if(!tabList[tmpurl?tmpurl:focusUrl]) setTabList({...tabList, [tmpurl?tmpurl:focusUrl]:{title:"test",num:1}})
+
+    },[serchParams])
+
+    useEffect(()=>{
+        if(Object.keys(tabList).length===0){
+            Update({ closing: true })
+            navigate("/")
+        }
+    },[tabList])
+
     return(
         <Window 
         Update = {Update}
@@ -18,18 +35,26 @@ function Browser(props, url_path){
         ClassName='Window Browser'
         Toolbar={(
             <div className="Browser-Tab-Container">
-                <div className='Tab Focused'>
-                    <div className='Tab-Info'>
-                        <div className='Tab-Title'>Blog</div>
-                        <Close/>
-                    </div>
-                </div>
-                <div className='Tab'>
-                    <div className='Tab-Info'>
-                        <div className='Tab-Title'>TestTab google</div>
-                        <Close/>
-                    </div>
-                </div>
+                {
+                    Object.keys(tabList).map(key=>{
+                        return(
+                            <div className={`Tab ${key===focusUrl && "Focused"}`} 
+                                 onClick={()=>setFocusUrl(key)}>
+                                <div className='Tab-Info'>
+                                    <div className='Tab-Title'>{key.replace("https://","").replace("http://","").split("/")[0]}</div>
+                                    <span onClick={(e)=>{
+                                            const tmpTabList={...tabList}
+                                            delete tmpTabList[key]
+                                            setTabList(tmpTabList)
+                                            setFocusUrl(Object.keys(tmpTabList).at(-1))
+                                            e.stopPropagation()
+                                        }
+                                    }><Close/></span>
+                                </div>
+                            </div> 
+                        )
+                    })
+                }
             </div>
         )}
         Contents={(
@@ -42,17 +67,19 @@ function Browser(props, url_path){
                 </div>
 
                 <div className='Browser-AddressBar'>
-                    <input type="text" value={url} spellCheck="false" />                    
+                    <input type="text" value={focusUrl} spellCheck="false" onChange={()=>false}/>                    
                 </div>
-                <a className='NewTab' href={url} target="_blank">
+                <a className='NewTab' href={focusUrl} target="_blank">
                     Open in a new tab
                 </a>
             </div>
 
             <div className='Browser'>
-                {/* <iframe src="https://www.google.com/?igu=1"></iframe> */}
-                {/* <iframe src="https://www.notion.so/devdori/Profile-b65bbb4fbb41417ab3c616dd84c43a28?pvs=4"></iframe> */}
-                <iframe src={url}></iframe>
+                {
+                    Object.keys(tabList).map(url=>{
+                        return(<iframe src={url} key={url} style={{display:url===focusUrl?"":"none"}}/>)
+                    })
+                }
             </div>
             </>
         )}
